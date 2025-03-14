9fa0d59d41b7c28b8688ca91a06a3144c3736e6d
Prem Sagar <prems@MacBook-Pro.local>
2025-03-14 15:36:42 +0530

    video on

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "off",
     screenshot: "off",
-    video: "off",
+    video: "on",
   },
 
   /* Configure projects for major browsers */
